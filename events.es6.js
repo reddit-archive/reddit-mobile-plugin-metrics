c30e4291cf9a52f03f464a30176aea313d7d5359
Jack Lawson <ajacksified@gmail.com>
2015-04-29 12:58:09 -0700

    Emit GA events on some app events; add dimensions for logged-in and compact mode.
    Track whether a user is logged in and in compact mode. Also watch events
    for toggling compact mode, voting, commenting, and searching.

--- events.es6.js
+++ events.es6.js
@@ -11,8 +11,34 @@ var events = {
     if (global && global.ga) {
       ga('set', 'page', fullUrl);
       ga('send', 'pageview');
+
+      var loggedIn = !!window.bootstrap.user;
+
+      var compactCookieValue = document.cookie.match(/\bcompact=(.+)\b/);
+      var compact = !!(compactCookieValue &&
+                      compactCookieValue.length > 1 &&
+                      compactCookieValue[1] === 'true');
+
+      ga('set', 'dimension2', loggedIn.toString());
+      ga('set', 'dimension3', compact.toString());
     }
-  }
+  },
+
+  'compactToggle': function (compact) {
+    ga('set', 'dimension3', compact.toString());
+  },
+
+  'vote': function (vote) {
+    ga('send', 'event', 'vote', vote.direction);
+  },
+
+  'comment': function (comment) {
+    ga('send', 'event', 'comment', comment.text.length);
+  },
+
+  'search': function (query) {
+    ga('send', 'event', 'search');
+  },
 };
 
 export default events;
